e04b80082e027af7ae3bce850295c3ac98ad4cd5
Cristiano Diedrich <cristiano@umov.me>
2019-10-07 13:05:21 -0300

    Adiciona função de update dos dados ja inseridos

--- run.sh
+++ run.sh
@@ -23,6 +23,49 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
+if [ "$1" = "update" ]; then
+    # Initialize PostgreSQL
+    createPostgresConfig
+    service postgresql start
+    sudo -u postgres createuser renderer
+    sudo -u postgres createdb -E UTF8 -O renderer gis
+    sudo -u postgres psql -d gis -c "CREATE EXTENSION postgis;"
+    sudo -u postgres psql -d gis -c "CREATE EXTENSION hstore;"
+    sudo -u postgres psql -d gis -c "ALTER TABLE geometry_columns OWNER TO renderer;"
+    sudo -u postgres psql -d gis -c "ALTER TABLE spatial_ref_sys OWNER TO renderer;"
+    setPostgresPassword
+
+    # Download Luxembourg as sample if no data is provided
+    if [ ! -f /data.osm.pbf ]; then
+        echo "WARNING: No import file at /data.osm.pbf, so importing Luxembourg as example..."
+        wget -nv http://download.geofabrik.de/europe/luxembourg-latest.osm.pbf -O /data.osm.pbf
+        wget -nv http://download.geofabrik.de/europe/luxembourg.poly -O /data.poly
+    fi
+
+    # determine and set osmosis_replication_timestamp (for consecutive updates)
+    osmium fileinfo /data.osm.pbf > /var/lib/mod_tile/data.osm.pbf.info
+    osmium fileinfo /data.osm.pbf | grep 'osmosis_replication_timestamp=' | cut -b35-44 > /var/lib/mod_tile/replication_timestamp.txt
+    REPLICATION_TIMESTAMP=$(cat /var/lib/mod_tile/replication_timestamp.txt)
+
+    # initial setup of osmosis workspace (for consecutive updates)
+    sudo -u renderer openstreetmap-tiles-update-expire $REPLICATION_TIMESTAMP
+
+    # copy polygon file if available
+    if [ -f /data.poly ]; then
+        sudo -u renderer cp /data.poly /var/lib/mod_tile/data.poly
+    fi
+
+    # Import data
+    sudo -u renderer osm2pgsql -d gis --append --slim -G --hstore --tag-transform-script /home/renderer/src/openstreetmap-carto/openstreetmap-carto.lua --number-processes ${THREADS:-4} ${OSM2PGSQL_EXTRA_ARGS} -S /home/renderer/src/openstreetmap-carto/openstreetmap-carto.style /data.osm.pbf
+
+    # Create indexes
+    sudo -u postgres psql -d gis -f indexes.sql
+
+    service postgresql stop
+
+    exit 0
+fi
+
 if [ "$1" = "import" ]; then
     # Initialize PostgreSQL
     createPostgresConfig
